219a802e522cc85ba508925fa1b28d8ef1fe2fc0
kristina.perova <chris.perowa@gmail.com>
2024-06-18 17:57:53 +0200

    created a hook and pass the state into the other components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import EditorField from "./components/EditorField";
 import PreviewerField from "./components/PreviewerField";
+import {useState} from "react";
 
 function App() {
+    const [text, setText] = useState('');
+
   return (
     <>
-      <EditorField />
-      <PreviewerField />
+      <EditorField
+          text={text}
+          setText={setText}
+      />
+      <PreviewerField
+          text={text}
+      />
     </>
   );
 }
