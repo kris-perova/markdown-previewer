ea76b5cd89de754e7a115d2a983583612bf3068c
kristina.perova <chris.perowa@gmail.com>
2024-06-19 14:16:22 +0200

    parsed the HTML string and converted into React elements:
    imported and implemented a library for sanitizing HTML-string and a library for parsing this string into the React elements;

--- src/components/PreviewerField.js
+++ src/components/PreviewerField.js
@@ -1,6 +1,8 @@
 import {marked} from "marked";
+import DOMPurify from "dompurify";
+import parse from "html-react-parser";
 
-const PreviewerField = ({text}) => {
+const PreviewerField = ({text}) => { // {Markdown text}
     // the first way - it's not recommended in react
     // useEffect(()=>{
     //     document.getElementById('content').innerHTML = marked.parse(text);
@@ -9,10 +11,16 @@ const PreviewerField = ({text}) => {
     // the second way:
     // document.getElementById('content').innerHTML = marked.parse(text);
 
+    const htmlString = marked.parse(text); // convert Markdown-text into html-string: <p>asd</p><h1>dsf</h1>
+    const cleanHtmlString = DOMPurify.sanitize(htmlString);
+
     return (
         <div className={'previewerField'}>
             <h3 className={'header'}>Previewer</h3>
-            <div className={'previewerText'} dangerouslySetInnerHTML={{__html: marked.parse(text)}}/>
+            {/*<div className={'previewerText'} dangerouslySetInnerHTML={{__html: cleanHtmlString}}/>*/}
+            <div>
+                {parse(cleanHtmlString)} {/* converting the string into the React element*/}
+            </div>
         </div>
     );
 
