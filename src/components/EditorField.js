6a71634b785a05de9cb44dca2efda08953eddda7
kristina.perova <chris.perowa@gmail.com>
2024-06-26 12:41:58 +0200

    renamed containers:
    renamed containers and class names

--- src/components/EditorField.js
+++ src/components/EditorField.js
@@ -1,17 +1,23 @@
 const EditorField = ({text, setText}) => {
+
     const handleChange = (e) => {
         setText(e.target.value);
     }
 
     return (
-        <div className={'editorField'}>
-            <h3 className={'header'}>Editor</h3>
-            <input
-                className={'textArea'}
-                type={'text'}
-                onChange={handleChange}
-                value={text} // устанавливается из состояния компонента [text]
-            />
+        <div className={'editorBlock'}>
+            <div>
+                <h3 className={'header'}>Editor</h3>
+            </div>
+
+            <div className={'editorField'}>
+                <textarea
+                    className={'inputArea'}
+                    onChange={handleChange}
+                    value={text} // устанавливается из состояния компонента [text]
+                />
+            </div>
+
         </div>
     );
 }
