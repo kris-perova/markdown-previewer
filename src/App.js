dbc92b64423242a72b2e846aa6950ef02cbf916b
kristina.perova <chris.perowa@gmail.com>
2024-06-26 12:38:36 +0200

    added containers:
    added 2 containers for css-styles

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import {useState} from "react";
 function App() {
     const [text, setText] = useState('');
 
-  return (
-    <div className={'generalContainer'}>
-      <EditorField
-          text={text}
-          setText={setText}
-      />
-      <PreviewerField
-          text={text}
-      />
-    </div>
-  );
+    return (
+        <div className={'outerContainer'}>
+            <div className={'generalContainer'}>
+                <EditorField
+                    text={text}
+                    setText={setText}
+                />
+                <PreviewerField
+                    text={text}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
