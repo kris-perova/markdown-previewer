708508dc3a042804573dcdf2982b073c8298b148
kristina.perova <chris.perowa@gmail.com>
2024-06-28 16:23:31 +0200

    added an image to the header

--- src/components/PreviewerField.js
+++ src/components/PreviewerField.js
@@ -1,6 +1,7 @@
 import {marked} from "marked";
 import DOMPurify from "dompurify";
 import parse from "html-react-parser";
+import view from "../icons/view.png"
 
 const PreviewerField = ({text}) => { // {Markdown text}
     // the first way - it's not recommended in react
@@ -15,10 +16,16 @@ const PreviewerField = ({text}) => { // {Markdown text}
     const cleanHtmlString = DOMPurify.sanitize(htmlString);
 
     return (
-        <div className={'previewerField'}>
-            <h3 className={'header'}>Previewer</h3>
+        <div className={'previewerBlock'}>
+            <div className={'header'}>
+                <img style={{width: "30px", height: "30px"}}
+                    src={view}
+                    alt="previewer text"/>
+                <h3>Previewer</h3>
+            </div>
+
             {/*<div className={'previewerText'} dangerouslySetInnerHTML={{__html: cleanHtmlString}}/>*/}
-            <div className={'textArea'}>
+            <div className={'viewArea'}>
                 {parse(cleanHtmlString)} {/* converting the string into the React element*/}
             </div>
         </div>
