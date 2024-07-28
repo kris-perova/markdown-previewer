e8f16dc5e1319da63076d5422c5cf81469176496
kristina.perova <chris.perowa@gmail.com>
2024-07-28 14:18:31 +0200

    added markdown sample, updated css styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import EditorField from "./components/EditorField";
 import PreviewerField from "./components/PreviewerField";
 import {useState} from "react";
+import {markdownSample} from './utils'
 
 function App() {
-    const [text, setText] = useState('');
+    const [text, setText] = useState(markdownSample);
 
     return (
         <div className={'outerContainer'}>
